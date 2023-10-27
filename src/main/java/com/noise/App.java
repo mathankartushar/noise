52baf4e77387bc2ebebe49e6b997698c350b4ccb
tushar <mathankartushar@gmail.com>
2023-10-27 15:21:05 +0530

    project4

---
+++ src/main/java/com/noise/App.java
@@ -0,0 +1,13 @@
+package com.noise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
